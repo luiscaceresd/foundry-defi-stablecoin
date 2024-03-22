62a7cdfa2ed9575f764be4f6c3558d2eeb2f237b
luiscaceresd <luiscaceresd97@gmail.com>
2024-03-21 23:00:51 -0300

    created my first test! testing depositcollateral emits event properly

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Test } from "forge-std/Test.sol";
-import { DeployDSC } from "../../script/DeployDSC.s.sol";
-import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
-import { DSCEngine } from "../../src/DSCEngine.sol";
-import { HelperConfig } from "../../script/HelperConfig.s.sol";
-import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract DSCEngineTest is Test {
+    /* Events */
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+
     DeployDSC deployer;
     DecentralizedStableCoin dsc;
     DSCEngine dsce;
@@ -20,17 +24,17 @@ contract DSCEngineTest is Test {
     address public USER = makeAddr("user");
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
     uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
-    
-    function setUp() public {
-      deployer = new DeployDSC();
-      (dsc, dsce, config) = deployer.run();
-      (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
 
-      ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     ///////////////////////////////////
-    //////Constructor Tests /////////// 
+    //////Constructor Tests ///////////
     //////////////////////////////////
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
@@ -57,11 +61,11 @@ contract DSCEngineTest is Test {
     }
 
     function testGetTokenAmountFromUsd() public {
-      uint256 usdAmount = 100 ether;
-      // 2000 / 100
-      uint256 expectedWeth = 0.05 ether;
-      uint256 actualWeth = dsce.getTokenAmountFromUsd(weth, usdAmount);
-      assertEq(actualWeth, expectedWeth);
+        uint256 usdAmount = 100 ether;
+        // 2000 / 100
+        uint256 expectedWeth = 0.05 ether;
+        uint256 actualWeth = dsce.getTokenAmountFromUsd(weth, usdAmount);
+        assertEq(actualWeth, expectedWeth);
     }
 
     //////////////////////////////
@@ -69,38 +73,55 @@ contract DSCEngineTest is Test {
     /////////////////////////////
 
     function testRevertsIfCollateralZero() public {
-      vm.startPrank(USER);
-      ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
 
-      vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
-      dsce.depositCollateral(weth, 0);
-      vm.stopPrank();
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 
     function testRevertsWithUnapprovedCollateral() public {
-      ERC20Mock ranToken = new ERC20Mock("RAN", "RAN", USER, AMOUNT_COLLATERAL);
-      vm.startPrank(USER);
-      vm.expectRevert(DSCEngine.DSCEngine__TokenNotAllowed.selector);
-      dsce.depositCollateral(address(ranToken), AMOUNT_COLLATERAL);
-      vm.stopPrank();
+        ERC20Mock ranToken = new ERC20Mock("RAN", "RAN", USER, AMOUNT_COLLATERAL);
+        vm.startPrank(USER);
+        vm.expectRevert(DSCEngine.DSCEngine__TokenNotAllowed.selector);
+        dsce.depositCollateral(address(ranToken), AMOUNT_COLLATERAL);
+        vm.stopPrank();
     }
 
     modifier depositedCollateral() {
-      vm.startPrank(USER);
-      ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
-      dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
-      vm.stopPrank();
-      _;
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        _;
     }
 
     function testCanDepositCollateralAndGetAccountInfo() public depositedCollateral {
-      (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
-      
-      uint256 expectedTotalDscMinted = 0;
-      uint256 expectedDepositAmount = dsce.getTokenAmountFromUsd(weth, collateralValueInUsd);
-      assertEq(totalDscMinted, expectedTotalDscMinted);
-      assertEq(AMOUNT_COLLATERAL, expectedDepositAmount);
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
+
+        uint256 expectedTotalDscMinted = 0;
+        uint256 expectedDepositAmount = dsce.getTokenAmountFromUsd(weth, collateralValueInUsd);
+        assertEq(totalDscMinted, expectedTotalDscMinted);
+        assertEq(AMOUNT_COLLATERAL, expectedDepositAmount);
     }
 
-    // 
+    function testDepositCollateralEmitsEvent() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        vm.expectEmit(true, true, true, false, address(dsce));
+        emit CollateralDeposited(USER,weth,AMOUNT_COLLATERAL);
+        dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
+    }
+
+    //  function testCollateralDepositEmitsOutput() public {
+    //     vm.recordLogs();
+    //     vm.startPrank(USER);
+    //     ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+    //     dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
+    //     Vm.Log[] memory entries = vm.getRecordedLogs();
+    //     bytes32 user = entries[1].topics[1];
+
+    //     assert(address(user) != address(0));
+    // }
 }
