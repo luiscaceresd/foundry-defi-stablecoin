0b8f97224f4e799bcc0a336ab65ee38c11afc87d
luiscaceresd <luiscaceresd97@gmail.com>
2024-04-24 22:17:08 -0300

    finished for now

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -18,7 +18,7 @@ library OracleLib {
     uint256 private constant TIMEOUT = 3 hours;
 
     function staleCheckLatestRoundData(AggregatorV3Interface priceFeed) public view returns (uint80, int256, uint256, uint256, uint80) {
-        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
+      (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
 
       uint256 secondsSince = block.timestamp - updatedAt;
       if(secondsSince > TIMEOUT) revert OracleLib__StalePrice();
